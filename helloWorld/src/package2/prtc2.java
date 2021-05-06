6b171252839f8e4664d6477331e8d0aa6aadc0dd
fsanli23 <sanlifatih23@gmail.com>
2021-05-06 00:29:33 -0500

    commited

---
+++ helloWorld/src/package2/prtc2.java
@@ -0,0 +1,19 @@
+package package2;
+
+public class prtc2 {
+ public static void main(String[] args) {
+	  
+	 System.out.println("fatih");
+	 
+	 
+	 System.out.println("mm");
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+}
+}
---
+++ helloWorld/src/practiceEclipse/practiceEclipse2.java
@@ -0,0 +1,18 @@
+package practiceEclipse;
+
+public class practiceEclipse2 {
+	 public static void main(String[] args) {
+		
+		
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}
+
+}
